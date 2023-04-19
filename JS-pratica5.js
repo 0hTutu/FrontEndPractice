4b91ab0b8952c0ac53d5060ee0f8777c3fe0e786
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-04-19 10:55:33 -0300

    um dos primeiros programas que fiz!
    Ele é uma linha de dialogo interativa entre um atendente de um suporte e o cliente

---
+++ JS-pratica5.js
@@ -0,0 +1,13 @@
+    alert("Olá, seja bem vindo.");
+    alert("Vou precisar dos seus dados.");
+    
+    let nome = prompt("Qual seu nome?");
+    alert("ok.");
+    
+    let idade = prompt("Quantos anos você tem?");
+    alert("uhum.");
+    
+    let origem = prompt("de onde você é?");
+    alert("ok, vamos confirmar seus dados de cadastro.");
+    
+    alert(`Você se chama ${nome}, tem ${anos} anos e vem de ${origem}`);
