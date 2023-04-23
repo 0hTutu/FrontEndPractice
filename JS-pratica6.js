7c901423d68a0c038d3a1431960b7ea3569ba71c
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-04-23 00:07:15 -0300

    Update JS-pratica6.js

--- JS-pratica6.js
+++ JS-pratica6.js
@@ -2,3 +2,13 @@
 exercicio: criar sistema simples pra organizar uma empresa de pesca ficticia
 requisitos: Separar os diferentes tipos de peixe por preco, nome e quantidade total
 */
+
+let peixes = [];
+
+function infoPeixes(nome, preco, quantidade){
+    peixes.push({
+        nome: nome,
+        preco: preco,
+        quantidade: quantidade
+    }); 
+}
