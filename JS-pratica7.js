0fb16347e2c1d1501504638a2e3436bd532e1ac1
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-04-24 03:38:37 -0300

    novo exercicio da semana!

---
+++ JS-pratica7.js
@@ -0,0 +1,12 @@
+/*
+EXERCICIO: Criar sistema simples separar os alunos por turma, idade e esportes
+as turmas são 101, 102, 103, 104 e 105 | as idades estão entre 16 a 18 anos | os esportes são vôlei, basquete e futebol | ao todo são 50 alunos, 10 pra cada turma
+// Definir as turmas, idades e esportes disponíveis
+// Definir uma classe para representar cada aluno
+// Criar um array para armazenar todos os alunos
+// Adicionar 50 alunos aleatórios ao array
+// Separar os alunos por turma
+// Separar os alunos por idade
+// Separar os alunos por esporte
+// Exibir os alunos por turma, idade e esporte
+*/
