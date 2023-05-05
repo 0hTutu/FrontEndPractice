f60d571d916e892ec3ed669eccd52a5d6b7f0e6d
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-05-04 21:02:46 -0300

    Update and rename TS-pratica.ts to TS-pratica0.ts

---
+++ TS-pratica0.ts
@@ -0,0 +1,9 @@
+// Definir algumas variáveis pra armazenar notas
+let nota1: number = 8
+let nota2: number = 6
+
+// Calcular uma média
+let media: number = (nota1 + nota2) / 2
+
+// Exibir o resultado
+console.log(`A média das notas ${nota1} e ${nota2} é ${media}.`)
