95d568ab520b0726ff9c2e27170b258a4d280cd9
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-05-19 18:51:49 -0300

    Update and rename JS-pratica4.html to JS-pratica4/JS-pratica4.js

---
+++ JS-pratica4/JS-pratica4.js
@@ -0,0 +1,12 @@
+
+            /*
+            let corpo = document.body
+            let p1 = document.getElementsByTagName('p')[1];
+            let d = document.getElementById('msg'); 
+            d.style.background = '#bf46ef';
+            //d.innerText('estou no aguardo...');
+            window.document.getElementById("msg").innerText = "Olá!"; //Grande demais
+            */
+let d = document.querySelector('div#msg');
+d.style.color = 'black'
+       
